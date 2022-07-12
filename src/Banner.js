8be28d2f4d14a0bc17aac6f6e5b2a982130cdb48
HARSHIT <bharshit468@gamail.com>
2022-07-12 18:04:30 +0530

    Done with Banner staff...

--- src/Banner.js
+++ src/Banner.js
@@ -1,11 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "./axios";
+import "./Banner.css";
+import requests from "./requests";
 
 function Banner() {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchDate() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
+    }
+    fetchDate();
+  }, []);
+
+  console.log(movie);
+
+  // function for trucated
+
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
+
   return (
-    <div>
-      <div></div>
-      <div></div>
-    </div>
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url(
+        "https://image.tmdb.org/t/p/original/${movie?.backdrop_path}"
+        )`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="banner_contents">
+        <h1 className="banner_title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+
+        <div className="banner_buttons">
+          <button className="banner_button">Play</button>
+          <button className="banner_button">My List</button>
+        </div>
+        <h1 className="banner_descrp">{truncate(movie?.overview, 150)}</h1>
+      </div>
+
+      {/*empty div is for fading  */}
+      <div className="banner--fadeButtom"></div>
+    </header>
   );
 }
 
